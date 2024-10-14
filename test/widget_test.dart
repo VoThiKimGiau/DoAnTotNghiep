042484b43ee5be00c7941425e6ff0c451f2edb98
hoangminhwer <131875396+hoangminhwer@users.noreply.github.com>
2024-10-14 13:43:41 +0700

    init(2)

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:datn_cntt304_bandogiadung/main.dart';
+import 'package:datn_cntt304_bandogiadung/views/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
