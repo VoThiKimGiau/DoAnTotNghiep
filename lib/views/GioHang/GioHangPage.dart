6d2bd5067858932ee6299c889c8c2d19bd532b88
hohai00 <hohai847@gmail.com>
2024-11-12 16:57:04 +0700

    Them thong bao xoa gio hang

--- lib/views/GioHang/GioHangPage.dart
+++ lib/views/GioHang/GioHangPage.dart
@@ -10,7 +10,7 @@ import '../../services/storage/storage_service.dart';
 import 'CheckoutPage.dart';
 
 class GioHangPage extends StatefulWidget {
-  final String maGioHang; // Thêm maGioHang để lấy thông tin giỏ hàng
+  final String maGioHang;
   final String? maKH;
 
   GioHangPage({required this.maGioHang, required this.maKH});
@@ -23,37 +23,34 @@ class _GioHangPageState extends State<GioHangPage> {
   late ChiTietGioHangController _controller;
   late StorageService _storageService;
   List<ChiTietGioHang> gioHangItems = [];
-  bool _isLoading = true; // Biến để theo dõi trạng thái tải
+  bool _isLoading = true;
   ChiTietSPController _chiTietSPController = ChiTietSPController();
 
   @override
   void initState() {
     super.initState();
     _controller = ChiTietGioHangController();
-    _storageService = StorageService(); // Khởi tạo StorageService
-    _fetchGioHangItems(); // Gọi hàm để lấy danh sách sản phẩm
+    _storageService = StorageService();
+    _fetchGioHangItems();
   }
 
-  // Hàm lấy danh sách sản phẩm trong giỏ hàng
   Future<void> _fetchGioHangItems() async {
     try {
       final items = await _controller.fetchListProduct(widget.maGioHang);
       setState(() {
         gioHangItems = items;
-        _isLoading = false; // Cập nhật trạng thái tải
+        _isLoading = false;
       });
     } catch (e) {
       setState(() {
-        _isLoading = false; // Cập nhật trạng thái tải
+        _isLoading = false;
       });
-      // Hiển thị thông báo lỗi
       ScaffoldMessenger.of(context).showSnackBar(
         SnackBar(content: Text('Lỗi: $e')),
       );
     }
   }
 
-  // Hàm tính tổng tiền của giỏ hàng
   double _tinhTongTien() {
     double tongTien = 0.0;
     for (var item in gioHangItems) {
@@ -62,7 +59,6 @@ class _GioHangPageState extends State<GioHangPage> {
     return tongTien;
   }
 
-  // Hàm điều hướng tới trang thanh toán
   void _datHang() {
     double tongTien = _tinhTongTien();
     Navigator.of(context).push(
@@ -75,11 +71,32 @@ class _GioHangPageState extends State<GioHangPage> {
     );
   }
 
-  // Hàm xóa toàn bộ giỏ hàng
   void _xoaTatCa() async {
-    await _controller.xoaTatCaGioHang(widget.maGioHang);
-    _fetchGioHangItems();
-    setState(() {});
+    bool? confirm = await showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: const Text('Xác nhận'),
+          content: const Text('Bạn có chắc chắn muốn xóa tất cả sản phẩm trong giỏ hàng không?'),
+          actions: [
+            TextButton(
+              child: const Text('Hủy'),
+              onPressed: () => Navigator.of(context).pop(false),
+            ),
+            TextButton(
+              child: const Text('Đồng ý'),
+              onPressed: () => Navigator.of(context).pop(true),
+            ),
+          ],
+        );
+      },
+    );
+
+    if (confirm == true) {
+      await _controller.xoaTatCaGioHang(widget.maGioHang);
+      _fetchGioHangItems();
+      setState(() {});
+    }
   }
 
   @override
@@ -117,121 +134,121 @@ class _GioHangPageState extends State<GioHangPage> {
         child: _isLoading
             ? const Center(child: CircularProgressIndicator())
             : gioHangItems.isEmpty
-                ? Center(
-                    child: Column(
-                      mainAxisSize: MainAxisSize.min,
-                      children: [
-                        Image.asset(
-                          'assets/icons/badge.png',
-                          width: 100,
-                          height: 100,
-                          fit: BoxFit.contain,
-                        ),
-                        const SizedBox(height: 20),
-                        const Text(
-                          'Giỏ hàng trống',
-                          style: TextStyle(
-                            fontSize: 24,
-                            fontWeight: FontWeight.bold,
-                          ),
-                        ),
-                        const SizedBox(height: 20),
-                        FilledButton(
-                          style: FilledButton.styleFrom(
-                            backgroundColor: AppColors.primaryColor,
-                          ),
-                          onPressed: () {
-                            Navigator.of(context).pop();
-                          },
-                          child: const Text('Trở về trang chủ'),
-                        ),
-                      ],
-                    ),
-                  )
-                : Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Expanded(
-                        child: ListView.builder(
-                          itemCount: gioHangItems.length,
-                          itemBuilder: (context, index) {
-                            final item = gioHangItems[index];
-                            final imageUrl =
-                                _storageService.getImageUrl(item.maCTSP);
+            ? Center(
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Image.asset(
+                'assets/icons/badge.png',
+                width: 100,
+                height: 100,
+                fit: BoxFit.contain,
+              ),
+              const SizedBox(height: 20),
+              const Text(
+                'Giỏ hàng trống',
+                style: TextStyle(
+                  fontSize: 24,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              const SizedBox(height: 20),
+              FilledButton(
+                style: FilledButton.styleFrom(
+                  backgroundColor: AppColors.primaryColor,
+                ),
+                onPressed: () {
+                  Navigator.of(context).pop();
+                },
+                child: const Text('Trở về trang chủ'),
+              ),
+            ],
+          ),
+        )
+            : Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Expanded(
+              child: ListView.builder(
+                itemCount: gioHangItems.length,
+                itemBuilder: (context, index) {
+                  final item = gioHangItems[index];
+                  final imageUrl =
+                  _storageService.getImageUrl(item.maCTSP);
 
-                            return CartItem(
-                              imageUrl: imageUrl,
-                              item: item,
-                              maKH: widget.maKH!,
-                              onDelete: () async {
-                                await _controller.xoaChiTietGioHang(
-                                  widget.maGioHang,
-                                  item.maCTSP,
-                                );
-                                await _fetchGioHangItems();
-                                setState(() {});
-                              },
-                              onDecrease: () async {
-                                if (item.soLuong > 1) {
-                                  await _controller.capnhatChiTietGioHang(
-                                    item.copyWith(
-                                      soLuong: item.soLuong - 1,
-                                    ),
-                                  );
-                                  await _fetchGioHangItems();
-                                  setState(() {});
-                                }
-                              },
-                              onIncreate: () async {
-                                await _controller.capnhatChiTietGioHang(
-                                  item.copyWith(
-                                    soLuong: item.soLuong + 1,
-                                  ),
-                                );
-                                await _fetchGioHangItems();
-                                setState(() {});
-                              },
-                            );
-                          },
-                        ),
-                      ),
-                      const SizedBox(height: 20),
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: [
-                          const Text(
-                            'Tổng cộng:',
-                            style: TextStyle(
-                              fontSize: 18,
-                              fontWeight: FontWeight.bold,
-                            ),
+                  return CartItem(
+                    imageUrl: imageUrl,
+                    item: item,
+                    maKH: widget.maKH!,
+                    onDelete: () async {
+                      await _controller.xoaChiTietGioHang(
+                        widget.maGioHang,
+                        item.maCTSP,
+                      );
+                      await _fetchGioHangItems();
+                      setState(() {});
+                    },
+                    onDecrease: () async {
+                      if (item.soLuong > 1) {
+                        await _controller.capnhatChiTietGioHang(
+                          item.copyWith(
+                            soLuong: item.soLuong - 1,
                           ),
-                          Text(
-                            '\$${_tinhTongTien().toStringAsFixed(2)}',
-                            style: const TextStyle(
-                              fontSize: 18,
-                              fontWeight: FontWeight.bold,
-                            ),
-                          ),
-                        ],
-                      ),
-                      const SizedBox(height: 20),
-                      ElevatedButton(
-                        onPressed: _datHang,
-                        style: ElevatedButton.styleFrom(
-                          backgroundColor: Colors.blue,
-                          padding: const EdgeInsets.symmetric(vertical: 15),
-                          minimumSize: const Size(double.infinity, 50),
+                        );
+                        await _fetchGioHangItems();
+                        setState(() {});
+                      }
+                    },
+                    onIncreate: () async {
+                      await _controller.capnhatChiTietGioHang(
+                        item.copyWith(
+                          soLuong: item.soLuong + 1,
                         ),
-                        child: const Text(
-                          'Đặt hàng',
-                          style: TextStyle(
-                            fontSize: 18,
-                          ),
-                        ),
-                      ),
-                    ],
+                      );
+                      await _fetchGioHangItems();
+                      setState(() {});
+                    },
+                  );
+                },
+              ),
+            ),
+            const SizedBox(height: 20),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                const Text(
+                  'Tổng cộng:',
+                  style: TextStyle(
+                    fontSize: 18,
+                    fontWeight: FontWeight.bold,
                   ),
+                ),
+                Text(
+                  '\$${_tinhTongTien().toStringAsFixed(2)}',
+                  style: const TextStyle(
+                    fontSize: 18,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ],
+            ),
+            const SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: _datHang,
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Colors.blue,
+                padding: const EdgeInsets.symmetric(vertical: 15),
+                minimumSize: const Size(double.infinity, 50),
+              ),
+              child: const Text(
+                'Đặt hàng',
+                style: TextStyle(
+                  fontSize: 18,
+                ),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
