4e069d77aec41241c3f8a37ab45ee583a5672d70
VoThiKimGiau <vothikimgiau16122003@gmail.com>
2025-05-07 14:47:31 +0700

    Update IpConfig.dart

--- lib/config/IpConfig.dart
+++ lib/config/IpConfig.dart
@@ -1,3 +1,3 @@
 class IpConfig {
-  static const String ipConfig = 'https://api-datn-cntt304-bandogiadung-pruduction1.up.railway.app/';
+  static const String ipConfig = 'https://api-datn-cntt304-bandogiadung.onrender.com/';
 }
